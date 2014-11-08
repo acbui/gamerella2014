374d54b65a13d73eb8061f0cb2bc30df414b196e
Ashwath George <ashwath.george@gmail.com>
2014-11-08 18:39:07 -0500

    The Prodigal Son Returns
    Added enemy movement, attacks, and animations.

--- gamerella2014/Assets/Scripts/Enemy.cs
+++ gamerella2014/Assets/Scripts/Enemy.cs
@@ -1,15 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour 
+{
+	private Vector3 player;
+	private Vector2 playerDirection;
+	private float deltaX;
+	private float deltaY;
+	public float speed = 1.0f;
+	private int wall;
+	private float distance;
+	private bool wait;
+	private float waitTime;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		wall = 1 << 8;
+
+		wait = false;
+		waitTime = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		player = GameObject.Find ("Player").transform.position;
+
+		distance = Vector2.Distance (player, transform.position);
+
+		if (waitTime > 0)
+		{
+			waitTime -= Time.deltaTime;
+		}
+		else
+		{
+			wait = false;
+		}
+
+		if (distance < 10 && !wait)
+		{
+			deltaX = player.x - transform.position.x;
+			deltaY = player.y - transform.position.y;
+			playerDirection = new Vector2 (deltaX, deltaY);
+			
+			if (!Physics2D.Raycast (transform.position, playerDirection, 5, wall))
+			{
+				rigidbody2D.AddForce (playerDirection.normalized * speed);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			wait = true;
+			waitTime = 5;
+		}
+	}
+
+	public bool isWaiting ()
+	{
+		return wait;
 	}
 }
---
+++ gamerella2014/Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimation : MonoBehaviour 
+{
+	private Animator anim;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (rigidbody2D.velocity.y > 0)
+		{
+			anim.SetBool ("WalkBack", true);
+			anim.SetBool ("Back", true);
+			anim.SetBool ("WalkFront", false);
+			anim.SetBool ("Front", false);
+		}
+		
+		if (rigidbody2D.velocity.y < 0)
+		{
+			anim.SetBool ("WalkBack", false);
+			anim.SetBool ("Back", false);
+			anim.SetBool ("WalkFront", true);
+			anim.SetBool ("Front", true);
+		}
+
+		if (rigidbody2D.velocity.magnitude == 0.0f)
+		{
+			anim.SetBool ("WalkBack", false);
+			anim.SetBool ("WalkFront", false);
+		}
+
+		if (gameObject.GetComponent <Enemy> ().isWaiting ())
+		{
+			anim.SetBool ("Attack", true);
+		}
+		else
+		{
+			anim.SetBool ("Attack", false);
+		}
+	}
+}
