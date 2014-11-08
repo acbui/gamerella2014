00f580fdeb16de07cf64fa07a641a27e72518150
acbui <acqbui@gmail.com>
2014-11-08 13:05:10 -0500

    Boss Script

--- gamerella2014/Assets/Scripts/Boss.cs
+++ gamerella2014/Assets/Scripts/Boss.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Boss : MonoBehaviour {
+public class Boss : MonoBehaviour 
+{
+	public float aHP;
+	public int aLevel; 
+	public GameObject aTarget; 
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		aHP = 100;
+		aTarget = GameObject.Find ("MouseTarget"); 
+		aLevel = 1; 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update () 
+	{
+		attack ();
+	}
+
+	void attack()
+	{
+		// Grab. Available from the start. 
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+		{
+			
+		}
+
+		// Rock throw. 
+		else if (Input.GetKeyDown (KeyCode.Alpha2))
+		{
+			if (aLevel > 1)
+			{
+				
+			}
+		}
+
+		// Laser. 
+		else if (Input.GetKeyDown (KeyCode.Alpha3))
+		{
+			if (aLevel > 2)
+			{
+				
+			}
+		}
+
+		else if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (aLevel > 3)
+			{
+
+			}
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+
 	}
 }
---
+++ gamerella2014/Assets/Scripts/MouseTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTarget : MonoBehaviour 
+{
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePos.z = 0;
+		gameObject.transform.position = mousePos;
+	}
+}
