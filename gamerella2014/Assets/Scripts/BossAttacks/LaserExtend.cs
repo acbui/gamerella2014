efc9e162838a307bd6e1cd2e93acb637605c0ea9
acbui <acqbui@gmail.com>
2014-11-08 16:54:30 -0500

    Working on Laser attack

---
+++ gamerella2014/Assets/Scripts/BossAttacks/LaserExtend.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserExtend : MonoBehaviour {
+	
+	public float maxLaserScale;  
+	public Vector3 laserTarget;
+	public float speed; 
+	public float delay; 
+
+	public laserRetract retractScript; 
+	public GameObject retract;
+
+	// make the laser face the direction of the mouse at creation
+	void Start () {
+		laserTarget = GameObject.Find ("MouseTarget").transform.position; 
+
+		retractScript = gameObject.GetComponentInParent<laserRetract> ();
+		retract = retractScript.gameObject; 
+		
+		Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - retract.transform.position;
+		diff.Normalize();
+		
+		float rot = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		retract.transform.rotation = Quaternion.Euler(0f, 0f, rot - 90);
+	}
+
+	void FixedUpdate()
+	{
+		transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (transform.localScale.x, maxLaserScale, transform.localScale.z), Time.deltaTime * speed); 
+		if (transform.position.y >= maxLaserScale - 0.05f)
+		{
+			StartCoroutine (delayRetract());
+		}
+	}
+
+	IEnumerator delayRetract()
+	{
+		yield return new WaitForSeconds (delay);
+		retractScript.enabled = true; 
+	}
+}
---
+++ gamerella2014/Assets/Scripts/BossAttacks/LaserRetract.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserRetract : MonoBehaviour {
+	
+	public float speed; 
+	public float delay; 
+	
+	// make the laser face the direction of the mouse at creation
+	void Start () {
+	}
+	
+	void FixedUpdate()
+	{
+		transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (transform.localScale.x, 0, transform.localScale.z), Time.deltaTime * speed); 
+		if (transform.position.y <= 0.05f)
+		{
+			StartCoroutine (delayDestroy());
+		}
+	}
+	
+	IEnumerator delayDestroy()
+	{
+		yield return new WaitForSeconds (delay);
+		Destroy (gameObject);
+	}
+}
