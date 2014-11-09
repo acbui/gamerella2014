be7f29dc31cdc9125eb95f4b721bd1704d3c75ca
Ashwath George <ashwath.george@gmail.com>
2014-11-09 12:02:09 -0500

    Added spawn script

---
+++ gamerella2014/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour 
+{
+	public GameObject enemy;
+	public float timer;
+	public int cap = 10;
+	private int count;
+
+	void Awake ()
+	{
+		timer = Time.time + 1;
+	}
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (timer < Time.time && count < cap)
+		{
+			Spawn ();
+			timer = Time.time + 1;
+			count++;
+		}
+	}
+
+	void Spawn ()
+	{
+		Instantiate (enemy, transform.position, Quaternion.identity);
+		Debug.Log ("Birthed a hero of light");
+	}
+}
